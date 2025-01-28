cb620fe793ed6d57ec087a1b8474b341447a76bd
Flavio Volpicella <flavio.volpicella@gmail.com>
2025-01-28 10:43:56 +0100

    tables fixate e migrata la prima migrazione pere inserire articolo

--- database/migrations/2025_01_27_172201_create_articles_table.php
+++ database/migrations/2025_01_27_172201_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->number('price');
+            $table->float('price');
             $table->text('description');
             $table->timestamps();
         });
