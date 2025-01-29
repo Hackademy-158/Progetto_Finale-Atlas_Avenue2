0bf296c175bd88405e7eb7354a1ae44bc3324c80
Alessio_Grieco <Alessiogrieco218@gmail.com>
2025-01-29 22:58:09 +0100

    Revisor Snippets Completati

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,11 +1,4 @@
 <x-layout>
-    @if (session()->has('success'))
-        <div class="row justify-content-center">
-            <div class="col-5 alert alert-success text-center shadow rounded">
-                {{ session('success') }}
-            </div>
-        </div>
-    @endif
     <div class="container-fluid pt-5">
         <div class="row">
             <div class="col-3">
@@ -19,39 +12,46 @@
 
         @if ($article_to_check)
             <div class="row justify-content-center pt-5">
-                <div class="col-md-8">
-                    <div class="row justify-content-center">
-                        @for ($i = 0; $i < 6; $i++)
-                            <div class="col-6 col-md-4 mb-4 text-center">
-                                <img src="https://picsum.photos/300" class="img-fluid rounded shadow"
-                                    alt="Immagine segnaposto">
+                <div class="col-md-10">
+                    <div class="card shadow">
+                        <div class="row g-0">
+                            <div class="col-md-6">
+                                <img src="https://picsum.photos/600/400" class="img-fluid rounded-start h-100 w-100 object-fit-cover" alt="Immagine articolo">
                             </div>
-                        @endfor
-                    </div>
-                </div>
+                            <div class="col-md-6">
+                                <div class="card-body d-flex flex-column justify-content-between h-100">
+                                    <div>
+                                        <h2 class="card-title mb-3">{{ $article_to_check->title }}</h2>
+                                        <p class="card-subtitle mb-2">
+                                            <span class="fw-bold">Venditore:</span> 
+                                            @auth {{ $article_to_check->user->name }} @endauth
+                                        </p>
+                                        <p class="card-subtitle mb-2">
+                                            <span class="fw-bold">Prezzo:</span> 
+                                            {{ $article_to_check->price }} €
+                                        </p>
+                                        <p class="card-subtitle mb-3 text-muted">
+                                            #{{ $article_to_check->category->name }}
+                                        </p>
+                                        <p class="card-text">{{ $article_to_check->description }}</p>
+                                    </div>
 
-                <div class="col-md-4 ps-4 d-flex flex-column justify-content-between">
-                    <div>
-                        <h1>{{ $article_to_check->title }}</h1>
-                        <h4>@auth {{ $article_to_check->user->name }} @endauth
-                        </h4>
-                        <h4>{{ $article_to_check->price }} €</h4>
-                        <h4 class="fs-italic text-muted">#{{ $article_to_check->category->name }}</h4>
-                        <h6>{{ $article_to_check->description }}</h6>
-                    </div>
+                                    <div class="d-flex justify-content-around mt-4">
+                                        <form action="{{ route('reject', ['article' => $article_to_check]) }}" method="POST">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button class="btn btn-danger py-2 px-5 fw-bold">Rifiuta</button>
+                                        </form>
 
-                    <div class="d-flex pb-4 justify-content-around">
-                        <form action="{{ route('reject', ['article' => $article_to_check]) }}" method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <button class="btn btn-danger py-2 px-5 fw-bold">Rifiuta</button>
-                        </form>
-
-                        <form action="{{ route('accept', ['article' => $article_to_check]) }}" method="POST">
-                            @csrf
-                            @method('PATCH')
-                            <button class="btn btn-success py-2 px-5 fw-bold">Accetta</button>
-                        </form>
+                                        <form action="{{ route('accept', ['article' => $article_to_check]) }}" method="POST">
+                                            @csrf
+                                            @method('PATCH')
+                                            <button class="btn btn-success py-2 px-5 fw-bold">Accetta</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
