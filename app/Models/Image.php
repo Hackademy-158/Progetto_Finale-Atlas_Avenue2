5b62ef9447fa5139fc86b4580ae1026113350370
Anna-Git-97 <annaandreazzoli@gmail.com>
2025-02-05 16:37:01 +0100

    risolta logica immagini

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -21,17 +21,17 @@ class Image extends Model
     public static function getUrlByFilePath($filePath, $w = null, $h = null){
 
 if (!$w && !$h) {
-return Storage :: url($filePath);
+return Storage::url($filePath);
 }
 $path = dirname($filePath);
 $filename = basename($filePath);
 $file = "{$path}/crop_{$w}x{$h}_{$filename}";
-return Storage :: url($file);
+return Storage::url($file);
 }
 
 public function getUrl($w = null, $h = null)
 {
 
-return self :: getUrlByFilePath($this->path, $w, $h);
+return self::getUrlByFilePath($this->path, $w, $h);
 }
 }
