5cc9004c0ab99ee3ff776beadef65b253bde6330
Matteo Pastena <unknownbatbat00@gmail.com>
2025-02-05 21:43:40 +0100

    User story 8 completata

--- app/Livewire/ArticleCreate.php
+++ app/Livewire/ArticleCreate.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Article;
 use Livewire\Component;
 use App\Models\Category;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use Livewire\WithFileUploads;
 use Livewire\Attributes\Validate;
@@ -77,9 +78,17 @@ class ArticleCreate extends Component
             foreach ($this->images as $image) {
                 $newFileName = "articles/{$this->article->id}";
                 $newImage = $this->article->images()->create(['path' => $image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path, 300, 300));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                //! vecchio codice 
+                // dispatch(new ResizeImage($newImage->path, 300, 300));
+                // dispatch(new GoogleVisionSafeSearch($newImage->id));
+                // dispatch(new GoogleVisionLabelImage($newImage->id));
+                // Nuovo codice
+                
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 300, 300), 
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id),
+                ])->dispatch($newImage->id);
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
