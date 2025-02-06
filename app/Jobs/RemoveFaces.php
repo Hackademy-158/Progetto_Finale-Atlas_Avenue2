b37020cc217e0f99bb88067a1b785fd659f35e45
Flavio Volpicella <flavio.volpicella@gmail.com>
2025-02-06 12:16:27 +0100

    merge

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Models\Image;
+use Spatie\Image\Image as SpatieImage;
 use Spatie\Image\Enums\Fit;
 use Spatie\Image\Enums\AlignPosition;
 use Illuminate\Queue\SerializesModels;
@@ -16,35 +17,35 @@ use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 class RemoveFaces implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-    
+
     public $article_image_id;
-    
+
     /**
-    * Create a new job instance.
-    */
+     * Create a new job instance.
+     */
     public function __construct($article_image_id)
     {
         $this->article_image_id = $article_image_id;
     }
-    
+
     /**
-    * Execute the job.
-    */
+     * Execute the job.
+     */
     public function handle(): void
     {
         $i = Image::find($this->article_image_id);
         if (!$i) {
             return;
         }
-        
+
         $srcPath = storage_path('app/public/' . $i->path);
         $image = file_get_contents($srcPath);
-        
+
         putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
-        
-        
-        $imageAnnotator = new ImageAnnotatorClient(); 
-        $response = $imageAnnotator->faceDetection($image); 
+
+
+        $imageAnnotator = new ImageAnnotatorClient();
+        $response = $imageAnnotator->faceDetection($image);
         $faces = $response->getFaceAnnotations();
 
 
@@ -56,27 +57,27 @@ class RemoveFaces implements ShouldQueue
             foreach ($vertices as $vertex) {
                 $bounds[] = [$vertex->getX(), $vertex->getY()];
             }
-        
+
             $w = $bounds[2][0] - $bounds[0][0];
             $h = $bounds[2][1] - $bounds[0][1];
-        
+
             $image = SpatieImage::load($srcPath);
 
-        $image->watermark(
-            base_path('resources/img/smile.png'),
-            AlignPosition:: TopLeft,
-            paddingX: $bounds[0][0],
-            paddingY: $bounds[0][1],
-            width: $w,
-            height: $h,
-            fit: Fit::Stretch
-        
-        );
+            $image->watermark(
+                base_path('public/img/article/covers'),
+                AlignPosition::TopLeft,
+                paddingX: $bounds[0][0],
+                paddingY: $bounds[0][1],
+                width: $w,
+                height: $h,
+                fit: Fit::Stretch
+
+            );
 
 
-        $image->save($srcPath);
+            $image->save($srcPath);
         }
-        
+
         $imageAnnotator->close();
     }
 }
