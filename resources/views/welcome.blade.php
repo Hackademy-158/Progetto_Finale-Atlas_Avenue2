c70365479d0895e9d04f09c5ef8a95a9edd9a7d1
Danilo Gesuito <danilo.gesuito@hotmail.it>
2025-02-07 18:17:04 +0100

    modificato il responsive della welcome e modificato il main.js per l'intersezione con il primo numero

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,7 @@
                                 class="bi bi-arrow-right ms-2" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd"
                                     d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-                            </svg>
-                        </a>
+                            </svg> </a>
                     </div>
                 </div>
             </div>
@@ -34,6 +33,7 @@
             </div>
         </div>
     </div>
+
     <div class="container">
         <div class="row justify-content-center">
             @forelse ($articles as $article)
@@ -46,8 +46,9 @@
                         <div class="col-12 text-center mt-3 mb-3">
                             <div class="col-12">
                                 @auth
-                                    <p>{{__('ui.home.no_articles')}}</p>
-                                    <a href="{{ route('article.create') }}" style="color:#198754">{{ __('ui.home.create_article')}}</a>
+                                    <p>{{ __('ui.home.no_articles') }}</p>
+                                    <a href="{{ route('article.create') }}"
+                                        style="color:#198754">{{ __('ui.home.create_article') }}</a>
                                 @endauth
                             </div>
                         </div>
@@ -57,8 +58,9 @@
                             <div class="col-12 text-center mt-3 mb-3">
                                 <div class="col-12">
                                     @guest
-                                        <p>{{__('ui.last_articles.no_articles')}}</p>
-                                        <a href="{{ route('register') }}" style="color:#198754"> {{ __('ui.articles.create_one')}} </a>
+                                        <p>{{ __('ui.last_articles.no_articles') }}</p>
+                                        <a href="{{ route('register') }}" style="color:#198754">
+                                            {{ __('ui.articles.create_one') }} </a>
                                     @endguest
                                 </div>
                             </div>
@@ -83,94 +85,117 @@
     <section>
         <div class="container-fluid position-relative">
             <div class="row">
-                <h1 class=" text-center fw-bold display-3 text-main mt-5 mb-3">{{__('ui.features.one_click')}} <span
-                        class="text-uppercase text-success">{{__('ui.features.big_click')}}</span></h1>
+                <h1 class=" text-center fw-bold display-3 text-main mt-5 mb-3">{{ __('ui.features.one_click') }}<span
+                        class="text-uppercase text-success">{{ __('ui.features.big_click') }}</span></h1>
             </div>
-            <div class="container-fluid position-relative">
-                <div class="row">
-                    <div class="col-lg-1 hero-content display-3 fw-bold mb-3 margin-right-0" style="text-align:right">
+
+            <div class="container-fluid position-relative mt-4 mb-2">
+                <div class="row justify-content-center align-items-center p-0">
+                    {{-- simbolo --}}
+                    <div class="simbol col-2 col-lg-1 display-3 fw-bold mb-3 margin-right-0" style="text-align:right">
                         <i class="bi bi-check-circle"></i>
                     </div>
-                    <div class=" col-lg-5 hero-content" style="margin-left:0; padding-left:0">
-                        <h3 class="display-3 fw-bold mb-3">{{__('ui.features.quality_work')}} <span class="text-success">{{__('ui.features.quality_green')}}</span></h3>
-                        <h4>{{__('ui.features.timely_delivery')}}</h4>
+                    {{-- testo --}}
+                    <div class="AltTitSot col-10 col-lg-5 d-flex flex-lg-column">
+                        <h3 class="titolo display-3 fw-bold">{{ __('ui.features.quality_work') }} <span
+                                class="text-success">{{ __('ui.features.quality_green') }}</span></h3>
+                        <h4 class="sottotitolo">{{ __('ui.features.timely_delivery') }}</h4>
                     </div>
-                    <div class="col-lg-5" style="padding-top:100px">
-                        <img style="border-radius: 25px" src="{{ asset('img/welcome/Lavoro.jpg') }}" alt="">
+                    {{-- immagine --}}
+                    <div class="col-12 col-lg-6 text-center">
+                        <img style="border-radius: 25px" class="img-fluid" src="{{ asset('img/welcome/Lavoro.jpg') }}"
+                            alt="">
                     </div>
                 </div>
-
-
             </div>
-            <div class="container-fluid position-relative">
-                <div class="row">
-                    <div class="col-lg-5" style="padding-top:100px; margin-left:30px">
-                        <img style="border-radius: 25px" src="{{ asset('img/welcome/Pagamenti.jpg') }}" alt="">
-                    </div>
-                    <div class="col-lg-1 hero-content display-3 fw-bold mb-3 margin-right-0" style="text-align:right">
+
+            <div class="container-fluid position-relative mt-4 mb-2">
+                <div class="row justify-content-center align-items-center p-0">
+                    {{-- simbolo --}}
+                    <div class="simbol col-2 col-md-1 display-3 fw-bold mb-3 margin-right-0" style="text-align:right">
                         <i class="bi bi-check-circle"></i>
                     </div>
+                    {{-- testo --}}
+                    <div class="AltTitSot col-10 col-md-5 d-flex flex-lg-column">
+                        <h3 class="titolo display-3 fw-bold ">{{ __('ui.features.protected_payments') }}<span
+                                class="text-success"> {{ __('ui.features.payment_green') }}</span></h3>
+                        <h4 class="sottotitolo">{{ __('ui.features.transaction_unlock') }}</h4>
+                    </div>
+                    {{-- immagine --}}
+                    <div class="col-12 col-md-6 text-center">
+                        <img style="border-radius: 25px" class="img-fluid" src="{{ asset('img/welcome/Pagamenti.jpg') }}" alt="">
+                    </div>
 
-                    <div class=" col-lg-5 hero-content" style="margin-left:0; padding-left:0">
-                        <h3 class="display-3 fw-bold mb-3">{{__('ui.features.protected_payments')}}<span class="text-success"> {{__('ui.features.payment_green')}}</span></h3>
-                        <h4>{{__('ui.features.transaction_unlock')}}</h4>
+                </div>
+            </div>
+
+            <div class="container-fluid position-relative mt-4 mb-2">
+                <div class="row justify-content-center align-items-center p-0">
+                    {{-- simbolo --}}
+                    <div class="simbol col-2 col-md-1 display-3 fw-bold mb-3 margin-right-0" style="text-align:right">
+                        <i class="bi bi-check-circle"></i>
+                    </div>
+                    {{-- testo --}}
+                    <div class="AltTitSot col-10 col-md-5 d-flex flex-lg-column">
+                        <h3 class="titolo display-3 fw-bold ">{{ __('ui.features.local_global') }}<span class="text-success"> {{ __('ui.features.local_green') }}</span>
+                        </h3>
+                        <h4 class="sottotitolo">{{ __('ui.features.choice_yours') }}</h4>
+                    </div>
+                    {{-- immagine --}}
+                    <div class="col-12 col-md-6 text-center">
+                        <img style="border-radius: 25px" class="img-fluid" src="{{ asset('img/welcome/Globale.jpg') }}" alt="">
                     </div>
                 </div>
+            </div>
 
-                <div class="container-fluid position-relative">
-                    <div class="row">
-                        <div class="col-lg-1 hero-content display-3 fw-bold mb-3 margin-right-0"
-                            style="text-align:right">
-                            <i class="bi bi-check-circle"></i>
-                        </div>
-                        <div class=" col-lg-5 hero-content" style="margin-left:0; padding-left:0">
-                            <h3 class="display-3 fw-bold mb-3">{{__('ui.features.local_global')}} <span class="text-success"> {{__('ui.features.local_green')}}</span>
-                            </h3>
-                            <h4>{{__('ui.features.choice_yours')}}</h4>
-                        </div>
-                        <div class="col-lg-5  " style="padding-top:100px">
-                            <img style="border-radius: 25px" src="{{ asset('img/welcome/Globale.jpg') }}" alt="">
-                        </div>
+            <div class="container-fluid position-relative mt-4 mb-2">
+                <div class="row justify-content-center align-items-center p-0">
+                    {{-- simbolo --}}
+                    <div class="simbol col-2 col-md-1 display-3 fw-bold mb-3 margin-right-0" style="text-align:right">
+                        <i class="bi bi-check-circle"></i>
                     </div>
-                    <div class="container-fluid position-relative">
-                        <div class="row">
-                            <div class="col-lg-5 mb-3" style="padding-top:100px; margin-left:25px ">
-                                <img style="border-radius: 25px" src="{{ asset('img/welcome/24H.jpg') }}" alt="">
-                            </div>
-                            <div class="col-lg-1 hero-content display-3 fw-bold mb-3 margin-right-0"
-                                style="text-align:right">
-                                <i class="bi bi-check-circle"></i>
-                            </div>
-                            <div class=" col-lg-5 hero-content" style="margin-left:0; padding-left:0">
-                                <h3 class="display-3 fw-bold mb-3">{{__('ui.features.support')}}<span class="text-success"> {{__('ui.features.support_green')}}</span>
-                                </h3>
-                                <h4>{{__('ui.features.support_description')}}</h4>
-                            </div>
-                        </div>
+                    {{-- testo --}}
+                    <div class="AltTitSot col-10 col-md-5 d-flex flex-lg-column">
+                        <h3 class="titolo display-3 fw-bold">{{ __('ui.features.support') }}<span class="text-success">
+                            {{ __('ui.features.support_green') }}</span>
+                        </h3>
+                        <h4 class="sottotitolo">{{ __('ui.features.support_description') }}</h4>
+                    </div>
+                    {{-- immagine --}}
+                    <div class="col-12 col-md-6 text-center">
+                        <img style="border-radius: 25px" class="img-fluid" src="{{ asset('img/welcome/24H.jpg') }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
     </section>
+
     <!-- SEZIONE NUMERI INCREMENTALI gestiti con js in main.js-->
     <section>
         <div class="container">
             <div class="row">
                 <div class="col-12 text-center mt-5 mb-5">
-                    <h2>{{__('ui.stats.title')}}</h2>
+                    <h2>{{ __('ui.stats.title') }}</h2>
                 </div>
             </div>
-            <div class="row">
+            <div class="row align-items-center flex-column flex-lg-row">
                 <div class="col-4 text-center">
-                    <img style="border-radius: 15px" src="{{ asset('img/welcome/Soddisfatta.jpg') }}" />
+                    <img style="border-radius: 15px" class="img-fluid"
+                        src="{{ asset('img/welcome/Soddisfatta.jpg') }}" />
                     <h3 id="firstNumber"></h3>
-                    <p>{{__('ui.stats.satisfied_clients')}}</p>
+                    <p>{{ __('ui.stats.satisfied_clients') }}</p>
                 </div>
                 <div class="col-4 text-center">
-                    <img style="border-radius: 15px" src="{{ asset('img/welcome/Pollice.jpg') }}" />
+                    <img style="border-radius: 15px" class="img-fluid"
+                        src="{{ asset('img/welcome/Pollice.jpg') }}" />
                     <h3 id="secondNumber"></h3>
-                    <p>{{__('ui.stats.positive_reviews')}}</p>
+                    <p>{{ __('ui.stats.positive_reviews') }}</p>
                 </div>
                 <div class="col-4 text-center">
-                    <img style="border-radius: 15px" src="{{ asset('img/welcome/Partner.jpg') }}" />
+                    <img style="border-radius: 15px" class="img-fluid"
+                        src="{{ asset('img/welcome/Partner.jpg') }}" />
                     <h3 id="thirdNumber"></h3>
-                    <p>{{__('ui.stats.partners')}}</p>
+                    <p>{{ __('ui.stats.partners') }}</p>
                 </div>
             </div>
         </div>
@@ -179,19 +204,20 @@
     <div class="spacer"></div>
     <div class="spacer"></div>
     </section>
+
     <!-- Sezione Our Team -->
     <div class="container">
         <div class="spacer"></div>
         <div class="spacer"></div>
         <div class="row">
-            <div class="col-sm-12 mt-5 mb-5">
-                <h3 class="text-center text-uppercase">{{__('ui.about.title')}}</h3>
+            <div class="col-sm-12 mt-5 mb-4">
+                <h3 class="text-center text-uppercase">{{ __('ui.about.title') }}</h3>
             </div>
         </div>
         <div class="spacer"></div>
         <div class="spacer"></div>
-        <div class="row">
-            <div class="col-sm-6 col-md-2 text-center mx-4 mb-5">
+        <div class="row justify-content-center mb-4">
+            <div class="col-sm-6 col-md-4 col-lg-2 text-center">
                 <img src="https://lipsum.app//id/07/150x150/" class="rounded-circle team-pic" alt="Cinque Terre">
                 <div class="spacer"></div>
                 <h3 class="text-success">Danilo</h3>
@@ -200,7 +226,7 @@
                 <i class="bi bi-linkedin  margin-right-20"></i>
                 <i class="bi bi-instagram"></i>
             </div>
-            <div class="col-sm-6 col-md-2 text-center mx-4">
+            <div class="col-sm-6 col-md-4 col-lg-2 text-center">
                 <img src="https://lipsum.app//id/07/150x150/" class="rounded-circle team-pic" alt="Cinque Terre">
                 <div class="spacer"></div>
                 <h3 class="text-success">Alessio</h3>
@@ -209,7 +235,7 @@
                 <i class="bi bi-linkedin  margin-right-20"></i>
                 <i class="bi bi-instagram "></i>
             </div>
-            <div class="col-sm-6 col-md-2 text-center mx-4">
+            <div class="col-sm-6 col-md-4 col-lg-2 text-center">
                 <img src="https://lipsum.app//id/07/150x150/" class="rounded-circle team-pic" alt="Cinque Terre">
                 <div class="spacer"></div>
                 <h3 class="text-success">Matteo</h3>
@@ -218,7 +244,7 @@
                 <i class="bi bi-linkedin  margin-right-20"></i>
                 <i class="bi bi-instagram "></i>
             </div>
-            <div class="col-sm-6 col-md-2 text-center mx-4">
+            <div class="col-sm-6 col-md-4 col-lg-2 text-center">
                 <img src="https://lipsum.app//id/07/150x150/" class="rounded-circle team-pic" alt="Cinque Terre">
                 <div class="spacer"></div>
                 <h3 class="text-success">Anna</h3>
@@ -227,7 +253,7 @@
                 <i class="bi bi-linkedin  margin-right-20"></i>
                 <i class="bi bi-instagram "></i>
             </div>
-            <div class="col-sm-6 col-md-2 text-center">
+            <div class="col-sm-6 col-md-4 col-lg-2 text-center">
                 <img src="https://lipsum.app//id/07/150x150/" class="rounded-circle team-pic" alt="Cinque Terre">
                 <div class="spacer"></div>
                 <h3 class="text-success">Flavio</h3>
